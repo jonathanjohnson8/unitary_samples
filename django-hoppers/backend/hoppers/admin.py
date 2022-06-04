e144a3e2b51cdbba9693a68db2f7bfc8edb1a177
jon <jjohn8@stanford.edu>
2022-06-04 19:17:13 -0400

    added serializer, created RESTAPI, and other things

--- django-hoppers/backend/hoppers/admin.py
+++ django-hoppers/backend/hoppers/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Hoppers
+
+class HoppersAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'completed')
+
+# Register your models here.
+
+admin.site.register(Hoppers, HoppersAdmin)
---
+++ django-hoppers/backend/hoppers/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import Hoppers
+
+class HoppersSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Hoppers
+        fields = ('id', 'title', 'description', 'completed')
